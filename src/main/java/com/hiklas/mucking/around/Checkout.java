d59fa4c7be6b198f871c8880096f77c7fd7506cf
Fiona Bianchi <fiona@hiklas.co.uk>
2016-12-04 21:01:34 +0000

    Initial skeleton code

---
+++ src/main/java/com/hiklas/mucking/around/Checkout.java
@@ -0,0 +1,12 @@
+package com.hiklas.mucking.around;
+
+/**
+ *
+ */
+public class Checkout
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/hiklas/mucking/around/CheckoutTest.java
@@ -0,0 +1,18 @@
+package com.hiklas.mucking.around;
+
+import org.junit.Test;
+
+
+/**
+ */
+public class CheckoutTest
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assert ( true );
+    }
+}
