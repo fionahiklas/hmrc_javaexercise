7d5c8f31ea148ad4ab548e36366c639a7b24e9ee
Fiona Bianchi <fiona@hiklas.co.uk>
2016-12-07 12:22:28 +0000

    Adding price list and basic implementation

---
+++ src/main/java/com/hiklas/mucking/around/ApplesAndOrangesPriceList.java
@@ -0,0 +1,7 @@
+package com.hiklas.mucking.around;
+
+/**
+ * Created by fiona on 07/12/2016.
+ */
+public class ApplesAndOrangesPriceList {
+}
---
+++ src/test/java/com/hiklas/mucking/around/ApplesAndOrangesPriceListTest.java
@@ -0,0 +1,28 @@
+package com.hiklas.mucking.around;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by fiona on 07/12/2016.
+ */
+public class ApplesAndOrangesPriceListTest {
+
+  private ApplesAndOrangesPriceList priceListInstance;
+
+  @Before
+  public void setup() {
+    priceListInstance = new ApplesAndOrangesPriceList();
+  }
+
+  @Test
+  public void testCreationOfPriceList() {
+    assertThat(priceListInstance, notNullValue());
+  }
+
+  
+}
